eea893a57e7fa18e2094544d90d278114f0a2955
Samuel Sabater <ssabaterf@gmail.com>
2024-02-11 00:40:18 +0100

    updated runtime

--- lib/init-stack.ts
+++ lib/init-stack.ts
@@ -1,6 +1,6 @@
+import { GoFunction } from "@aws-cdk/aws-lambda-go-alpha";
 import * as cdk from "aws-cdk-lib";
 import { LambdaIntegration, RestApi } from "aws-cdk-lib/aws-apigateway";
-import * as lambda from "aws-cdk-lib/aws-lambda";
 import { Construct } from "constructs";
 
 export class GoBEStack extends cdk.Stack {
@@ -8,16 +8,26 @@ export class GoBEStack extends cdk.Stack {
     super(scope, id, props);
 
     // The code that defines your stack goes here
-    const regionInfoF = new lambda.Function(this, "region-info", {
-      runtime: lambda.Runtime.GO_1_X,
-      handler: "main",
-      code: lambda.Code.fromAsset("src/region_info"),
+    const regionInfoF = new GoFunction(this, "region-info", {
+      entry: "src/region_info",
+      bundling: {
+        goBuildFlags: ['-ldflags "-s -w"'],
+        environment: {
+          GOOS: "linux",
+          GOARCH: "amd64",
+        },
+      },
     });
 
-    const pingF = new lambda.Function(this, "ping", {
-      runtime: lambda.Runtime.GO_1_X,
-      handler: "main",
-      code: lambda.Code.fromAsset("src/ping"),
+    const pingF = new GoFunction(this, "ping", {
+      entry: "src/ping",
+      bundling: {
+        goBuildFlags: ['-ldflags "-s -w"'],
+        environment: {
+          GOOS: "linux",
+          GOARCH: "amd64",
+        },
+      },
     });
 
     const gateway = new RestApi(this, "rest-http-api", {
